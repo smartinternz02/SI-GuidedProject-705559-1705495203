1a0af0f4ca3bae1b2746263d17073a9bf10c3958
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-02-02 22:08:44 +0530

    Added Jenkins Integrations

---
+++ AMAZON AUT/Test Suites/AMAZON_AUT_JenkinsIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AMAZON_AUT_JenkinsIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>abf28c68-5485-4bce-9b24-b1272049c4c0</testSuiteGuid>
+   <testCaseLink>
+      <guid>2361e3a6-1daf-4baf-bd33-5064bfea0a01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/012_TC_AMAZON_JenkinsIntegration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>

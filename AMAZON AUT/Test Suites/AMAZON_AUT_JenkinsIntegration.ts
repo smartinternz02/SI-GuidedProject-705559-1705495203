4610a22c8cc6c7ab425eb55582a0a4da1a993b60
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-02-04 01:34:31 +0530

    Integrated Jenkins with Git and Katalon

---
+++ AMAZON AUT/Test Suites/AMAZON_AUT_JenkinsIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test Suite to Validate Jenkins Integration With Git And Katalon</description>
+   <name>AMAZON_AUT_JenkinsIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1b5ecf26-27d8-410a-98d8-a8db461a6fc6</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ec0ea78-e12f-433a-a015-4e3dc3789e46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/012_TC_AMAZON_JenkinsIntegration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
